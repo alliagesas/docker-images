0f04df1bf381d9c0608e1a0df5cc0e7c8929eeb5
vincent <vincent.couchouron@pingat.com>
2026-02-28 08:47:46 +0100

    Nouvelle installation de Docker (Debian >=12)

--- install-docker.sh
+++ install-docker.sh
@@ -5,32 +5,24 @@ apt-get update && apt-get upgrade -y
 # Suppression des paquets existants
 apt-get remove docker docker-engine docker.io containerd runc
 
-# Installer Docker via Docker's repository
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+# Add Docker's official GPG key:
+apt update
+apt install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add Docker’s official GPG key:
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-#curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+# Add the repository to Apt sources:
+tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
 
-# Add Docker's repository
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-#add-apt-repository \
-#   "deb [arch=amd64] https://download.docker.com/linux/debian \
-#   $(lsb_release -cs) \
-#   stable"
+apt update
+
 
 # Install Docker package
-apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io
-
-# Install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-docker-compose --version
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
